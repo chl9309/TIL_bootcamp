8731b2fe136af567d18830d554c5cdd6462a1a20
최홍준[부울경_1반] <chl9309@gmail.com>
2022-07-26 14:11:34 +0900

    0726 workshop

--- 0726/WorkShop/WorkShop.md
+++ 0726/WorkShop/WorkShop.md
@@ -5,22 +5,96 @@
 문자열을 전달 받아 해당 문자열에서 중복해서 나타난 문자들을 담은 list를 반환하는 duplicated_letters 함수를 작성하시오.
 
 ```python
-duplicated_letters(‘apple’) # => [‘p’]
-duplicated_letters(‘banana’) # => [’a’, ‘n’]
+def duplicated_letters(data):
+
+    data_list = data.split()
+    duplicated_no = []
+    duplicated_yes = []
+
+    for i in range(len(data_list)):
+
+        for j in range(len(duplicated_no)):
+
+            if data_list[i] == duplicated_no[j]:
+
+                duplicated_yes.append(data_list[i])
+            
+            else:
+
+                duplicated_no.append(data_list[i])
+
+    return duplicated_yes
+
+
+
+duplicated_letters('apple') # => ['p']
+duplicated_letters('banana') # => ['a', 'n']
 ```
+
+> 작동이 안되므로 수정 필요
+> 이 알고리즘으론 중복이 3개 이상이면 그 중복 횟수만큼 출력을 함
+
+
+
 
 ### 소대소대
 문자열을 전달 받아 해당 문자열을 소문자와 대문자가 번갈아 나타나도록 변환하여 반환하는 low_and_up 함수를 작성하시오. 이때, 전달 받는 문자열은 알파벳으로만 구성된다.
 
 ```python
-low_and_up(‘apple’) # => aPpLe
-low_and_up(‘banana’) # => bAnAnA
+def low_and_up(data):
+
+    data = list(data)
+    result= []
+
+    for i in range(len(data)):
+        
+        j = data[i]
+        
+        if i % 2:
+            result.append(j.upper())
+            
+        else:
+            result.append(j.lower())
+    
+    return result
+
+
+low_and_up('apple') # => aPpLe
+low_and_up('banana') # => bAnAnA
 ```
+
+
+
+
+
+
+
+
 
 ### 솔로 천국 만들기
 정수 0부터 9까지로 이루어진 list를 전달 받아, 연속적으로 나타나는 숫자는 하나만 남 기고 제거한 list를 반환하는 lonely 함수를 작성하시오. 이때, 제거된 후 남은 수들이 담긴 list의 요소들은 기존의 순서를 유지해야 한다.
 
 ```python
+def lonely(data):
+
+    result = []
+    result.append(data[0])
+    for i in range(1,len(data)):
+
+        if i == 0:
+            result.append(data[i])
+
+        else:
+            if data[i] == data[i-1]:
+
+                pass
+            else:
+                result.append(data[i])
+
+    return result
+
+
+
 lonely([1, 1, 3, 3, 0, 1, 1]) # => [1, 3, 0, 1]
 lonely([4, 4, 4, 3, 3]) # => [4, 3]
 ```
--- test.py
+++ test.py
@@ -7,41 +7,131 @@
 #print(div)
 
 
+#
+#def leap_year(year):
+#
+#    this_year = True
+#
+#    if year % 4:
+#
+#        this_year = False
+#
+#    else:
+#
+#        if year % 100:
+#
+#            this_year = True
+#
+#        else:
+#
+#            if year % 400:
+#
+#                this_year = False
+#            
+#            else:
+#
+#                this_year = True
+#
+#    if this_year:
+#
+#        result = f'{year}은 윤년입니다.'
+#
+#    else:
+#
+#        result = f'{year}은 윤년이 아닙니다'
+#
+#    return result
+#
+#
+#print(leap_year(2021))
+#print(leap_year(2020))
 
-def leap_year(year):
 
-    this_year = True
 
-    if year % 4:
 
-        this_year = False
 
-    else:
 
-        if year % 100:
 
-            this_year = True
+
+#def duplicated_letters(data):
+#
+#    data_list = data.split()
+#    duplicated_no = []
+#    duplicated_yes = []
+#
+#    for i in range(len(data_list)):
+#
+#        for j in range(len(duplicated_no)):
+#
+#            if data_list[i] == duplicated_no[j]:
+#
+#                duplicated_yes.append(data_list[i])
+#            
+#            else:
+#
+#                duplicated_no.append(data_list[i])
+#
+#    return duplicated_yes
+#
+#
+#
+#print(duplicated_letters('apple')) # => ['p']
+#print(duplicated_letters('banana')) # => ['a', 'n']
+
+
+
+
+#def low_and_up(dataorigin):
+#
+#    data = dataorigin.split()
+#    result= []
+#
+#    for i in range(len(data)):
+#        
+#        j = data[i]
+#        if i % 2:
+#
+#            result.append(j.lower())
+#            
+#        
+#        else:
+#
+#            result.append(j.upper())
+#            
+#    
+#    return result
+#
+#
+#print(low_and_up('apple')) # => aPpLe
+#print(low_and_up('banana')) # => bAnAnA
+
+
+
+def lonely(data):
+
+    result = []
+    
+    i= 0
+
+    while i < len(data):
+
+        if i == 0:
+            
+            result.append(data[i])
 
         else:
-
-            if year % 400:
-
-                this_year = False
             
+            if data[i] == result[i-1]:
+
+                pass
             else:
+                result.append(data[i])
 
-                this_year = True
-
-    if this_year:
-
-        result = f'{year}은 윤년입니다.'
-
-    else:
-
-        result = f'{year}은 윤년이 아닙니다'
+        i += 1
 
     return result
 
 
-print(leap_year(2021))
-print(leap_year(2020))
+
+lonely([1, 1, 3, 3, 0, 1, 1]) # => [1, 3, 0, 1]
+lonely([4, 4, 4, 3, 3]) # => [4, 3]
