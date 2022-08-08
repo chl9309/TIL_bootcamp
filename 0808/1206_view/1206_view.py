37dd0f4765a56387f3312573528e3624dfdcc174
최홍준[부울경_1반] <chl9309@gmail.com>
2022-08-09 01:30:09 +0900

    0808 homework..ing

---
+++ 0808/1206_view/1206_view.py
@@ -0,0 +1,49 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+T = 10
+
+for test_case in range(1, T + 1):
+
+    N = int(input())
+    A = list(map(int, input().split()))
+    result = 0
+    big = 0
+    for i in range(3, N-1):
+
+        if (A[i] - A[i-2]) > 0:
+            if (A[i] - A[i-1]) > 0:
+                if (A[i] - A[i+1]) > 0:
+                    if (A[i] - A[i+2]) > 0:
+
+                        if A[i-2] < A[i-1]:
+                            if A[i-1] < A[i+1]:
+                                if A[i+1] < A[i+2]:
+                                    big = A[i+2]
+                                else:
+                                    big = A[i+1]
+                            else:
+                                if A[i-1] < A[i+2]:
+                                    big = A[i+2]
+                                else:
+                                    big = A[i-1]
+                        else:
+                            if A[i-2] < A[i+1]:
+                                if A[i+1] < A[i+2]:
+                                    big = A[i+2]
+                                else:
+                                    big = A[i+1]
+                            else:
+                                if A[i-2] < A[i+2]:
+                                    big = A[i+2]
+                                else:
+                                    big = A[i-2]
+
+                        result += (A[i] - big)
+
+    print(f'#{test_case} {result}')
+
+
+
+    # 값 에러 뜨므로 내일 수정예정
