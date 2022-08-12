08a82ce7e5af9cea0fd9cc294a9e2dfb7f27b226
최홍준[부울경_1반] <chl9309@gmail.com>
2022-08-12 17:07:43 +0900

    0812 1221 GNS

---
+++ 0812/1213_string/1213_string_kmp.py
@@ -0,0 +1,57 @@
+import sys
+sys.stdin = open('test_input.txt', encoding='utf-8')
+
+def make_lps(p):
+    # 중계 리스트
+    lps = [0] * len(p)
+
+    # lps를 위한 prefix의 index
+    j = 0
+    # 전체 순회는
+
+    for i in range(1, len(p)):
+        # prefix와 suffix가 같을때
+        if p[j] == p[i]:
+            lps[i] = j + 1
+            j += 1
+
+        #다르면, j를 0으로 초기화
+        # pattern의 첫 글자와 다시 비교하도록 만들어야 한다.
+
+        # 현재 순회중인 자리 (i)와 0번째로 돌아간 pattern이
+        # 또 다시 일치하는지 확인
+
+    return lps
+
+
+def KMP(search, target):
+    lps = make_lps(search)
+
+    s_idx, t_idx = 0, 0
+    count = 0
+
+    while t_idx < len(target):
+        if search[s_idx] == target[t_idx]:
+            t_idx += 1
+            s_idx += 1
+        else:
+            # s_idx가 0이 아니라면
+            if s_idx != 0:
+                # lps를 써서 다음 조사 대상 수색
+                s_idx = lps[s_idx - 1]
+
+            #p_idx == 0 이라면
+            #t_idx를 한 칸 증가시켜서
+            #search의 첫번째 글자와 다음 target 조사대상이랑 바꾼다
+            else:
+                t_idx += 1
+    return count
+
+
+for test_case in range(10):
+    tc = int(input())
+    search = input()
+    target = input()
+
+    result = KMP(search, target)
+    print(result)M,./
---
+++ 0812/1221_GNS/1221_myGNS.py
@@ -0,0 +1,73 @@
+import sys
+
+sys.stdin = open('GNS_test_input.txt')
+
+T = int(input())
+
+for test_case in range(T):
+
+    trash, l = map(str, input().split())
+    l = int(l)
+
+    target = list(input().split())
+    translate = [0] * l
+    result = [0] * l
+    for i in range(l):
+
+        if target[i] =="ZRO" :
+            translate[i] = 0
+        elif target[i] =="ONE":
+            translate[i] = 1
+        elif target[i] =="TWO":
+            translate[i] = 2
+        elif target[i] =="THR":
+            translate[i] = 3
+        elif target[i] =="FOR":
+            translate[i] = 4
+        elif target[i] =="FIV":
+            translate[i] = 5
+        elif target[i] =="SIX":
+            translate[i] = 6
+        elif target[i] =="SVN":
+            translate[i] = 7
+        elif target[i] =="EGT":
+            translate[i] = 8
+        elif target[i] =="NIN":
+            translate[i] = 9
+
+
+    for x in range(l-1, 1, -1):
+        for y in range(x):
+            if translate[y] > translate[y+1]:
+                translate[y], translate[y+1] = translate[y+1], translate[y]
+
+
+    for j in range(l):
+        if translate[j] == 0:
+            result[j] = "ZRO"
+        elif translate[j] == 1:
+            result[j] = "ONE"
+        elif translate[j] == 2:
+            result[j] = "TWO"
+        elif translate[j] == 3:
+            result[j] = "THR"
+        elif translate[j] == 4:
+            result[j] = "FOR"
+        elif translate[j] == 5:
+            result[j] = "FIV"
+        elif translate[j] == 6:
+            result[j] = "SIX"
+        elif translate[j] == 7:
+            result[j] = "SVN"
+        elif translate[j] == 8:
+            result[j] = "EGT"
+        elif translate[j] == 9:
+            result[j] = "NIN"
+
+
+
+    print(f'{trash}')
+    for k in range(l):
+        print(f'{result[k]} ', end='')
+
+
