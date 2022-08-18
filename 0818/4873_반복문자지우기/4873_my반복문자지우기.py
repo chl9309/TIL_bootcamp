970693df80b27f821f05dabb53cf90489da5381c
최홍준[부울경_1반] <chl9309@gmail.com>
2022-08-18 13:58:43 +0900

    0818 4873반복문자지우기

--- 0818/4873_반복문자지우기/4873_my반복문자지우기.py
+++ 0818/4873_반복문자지우기/4873_my반복문자지우기.py
@@ -1,7 +1,22 @@
 import sys
-sys.stdin = open('input.txt')
-
+sys.stdin = open('input.txt', encoding='utf-8')
 T = int(input())
+
 for test_case in range(T):
 
-    
+    target = input()
+    tmp = []
+
+    for i in range(len(target)):
+
+        if not tmp:
+            tmp.append(target[i])
+
+        elif target[i] == tmp[-1]:
+            tmp.pop(-1)
+
+        else:
+            tmp.append(target[i])
+
+    result = len(tmp)
+    print(f'#{test_case+1} {result}')
