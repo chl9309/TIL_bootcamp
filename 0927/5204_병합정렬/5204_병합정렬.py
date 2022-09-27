4a387207ec33c1822e1b4c9fc27599cc81b3458b
최홍준 <chl9309@gmail.com>
2022-09-27 09:51:55 +0900

    0927

---
+++ 0927/5204_병합정렬/5204_병합정렬.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+
+def merge(arr):
+    if len(arr) == 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge(left)
+    right = merge(right)
+
+    l_idx = r_idx = k = 0
+    while l_idx < len(left) and r_idx < len(right):
+        
+
+T = int(input())
+for test_case in range(T):
+
+    N = int(input())
+    nums = list(map(int, input().split()))
+
+    result = 0
+
+
+
+
+
+
