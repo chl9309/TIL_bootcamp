67c72bfb9691f0f94ac9450b2438126961838458
최홍준 <chl9309@gmail.com>
2022-09-29 12:06:55 +0900

    0929 5248_그룹나누기

---
+++ 0929/5248_그룹나누기/5248_그룹나누기.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+for test_case in range(T):
+
+    N, M = map(int, input().split())
+    data = list(map(int, input().split()))
+    student = [{i+1} for i in range(N)]
+
+    data2 = []
+    for i in range(0, 2*M, 2):
+        data2.append([data[i], data[i+1]])
+
+    for i in range(len(data2)):
+        flag = False
+
+        for j in range(len(student)):
+            if data2[i][0] in student[j]:
+
+                for k in range(len(student)):
+                    if (data2[i][1] in student[k]) and j != k:
+
+                        student[j] = student[j].union(student[k])
+                        student.remove(student[k])
+
+                        flag = True
+                        break
+            if flag:
+                break
+
+    result = len(student)
+    print(f'#{test_case+1} {result}')
